4c48a83c3a9bb74c5c1860da36942657565c417d
atik <atiknugrah4gmail.com>
2023-05-07 17:41:29 +0700

    update persegi

---
+++ persegi/main.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  double panjang, lebar;
+  panjang = 20;
+  lebar = 12;
+  var persegiPanjang = panjang * lebar;
+  print(persegiPanjang);
+}
---
+++ usia/main.dart
@@ -0,0 +1,12 @@
+class Usia {
+  String? Nama;
+  int? umur;
+  Usia(String name, int Umur);
+}
+
+int y = 5;
+Container(
+  child: y >= 10?   
+  text('atik');
+  text('anak-anak');
+)
